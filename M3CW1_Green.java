77504c72e1a7f01cd90085ed4da9a146bfc290dd
JavonG-83 <greenj8783@student.faytechcc.edu>
2025-10-01 17:29:37 +0000

    WE DOING STUFF

---
+++ M3CW1_Green.java
@@ -0,0 +1,19 @@
+// M3CW1 Assignment
+// your class name should match your file name
+// Be sure to make this happen when you create the java file
+public class M3CW1_Green
+{
+public static void main(String[] args)
+{
+System.out.println("Javon"); // replace first name with your first name
+System.out.println("Green"); // replace last name with your last name
+System.out.println("2025 Fall Schedule"); // title of following course schedule
+System.out.println("CIS-115-0001 Intro to Programming and Logic");
+// example would be CIS-115-0001 Intro to Programming and Logic
+// repeat as needed for each course
+System.out.println("List course prefix, number and section as well as course title");
+System.out.println("List course prefix, number and section as well as course title");
+System.out.println("List course prefix, number and section as well as course title");
+System.out.println("State your programming experience and what if any language you know");
+}
+}
---
+++ M4CW1_Green.jav
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class M4CW1_Green
+{
+public static void main(String[] args)
+{
+int majors;
+int nonmajors;
+double total;
+double percentMajors;
+double percentNonmajors;
+// Get the number of majors.
+System.out.print("Enter the number of students majoring in CS: ");
+majors = input.nextInt();
+// Get the number of nonmajors.
+Display "Enter the number of non-computer science students:"
+Input nonmajors
+// Calculate the total number of students.
+Set total = majors + nonmajors
+// Calculate the percentage of majors.
+Set percentMajors = majors / total * 100
+// Calculate the percentage of nonmajors.
+Set percentNonmajors = nonmajors / total * 100
+// Display the percentage of majors.
+Display "Majors: ", percentMajors, "%"
+// Display the percentage of nonmajors.
+Display "Nonmajors: ", percentNonmajors, "%"
+}
+}
